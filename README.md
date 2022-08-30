19ecb4f9333068b1bbca50fcac2b6ad194968ac0
VasylArtymovych <art777vasss@gmail.com>
2022-08-30 11:41:08 +0300

    add nongoDB

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-To use weather server you need to provide:
-latitude
-longitude
-
-To use contacts server use url: localhost:
-
-//====== creat multiple ambaded dir's ==========
-// fs.mkdir(`${process.cwd()}/db/db2`, { recursive: true }, (error) => {
-// if (error) throw Error;
-// console.log("folder created");
-// });
-//====== delete multiple ambaded dir's ==========
-// fs.rmdir(`${process.cwd()}/db/db2`, { recursive: true }, (error) => {
-// if (error) throw Error;
-// console.log("folder created");
-// });
+run node server.js to start work with nongoDB
---
+++ db/connection.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+
+const url = process.env.MONGO_URI;
+const client = new MongoClient(url);
+// Database Name
+const dbName = "db-contacts";
+
+let collection = {};
+const getCollection = () => {
+  return collection;
+};
+
+async function connectMongo() {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    collection = db.collection("contacts");
+  } catch (error) {
+    console.error("Connection to MongoDB Atlas failed!", error);
+    process.exit();
+  }
+}
+
+module.exports = {
+  connectMongo,
+  getCollection,
+};
